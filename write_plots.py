2955a630a2adae0e24b2b245c62ff0c9ce278cfc
Eduardo Leao <eduardo.leao@stoneage.com.br>
2022-01-31 16:46:39 -0300

    estagio de acompanhamento de plots no dvc

---
+++ write_plots.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+  with open(sys.argv[1], 'r+') as logfile:
+    lines = logfile.readlines()
+
+  with open("loss.csv", "w+") as metricsfile:
+    metricsfile.write("epoch, iters, G_GAN, G_L1, D_real, D_fake\n")
+    for line in lines[1:]:
+      new_line = line.replace(",","").strip("()")
+      words = new_line.split()
+      metricsfile.write(f"{int(words[1])}, {int(words[3])}, {float(words[9])}, {float(words[11])}, {float(words[13])}, {float(words[15])}\n")
